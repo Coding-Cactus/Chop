4a72f3258dbdf2f80c37efb165f2a8fe8f21db9d
Coding-Cactus <codingcactus.cc@gmail.com>
2023-03-13 20:00:57 +0000

    :sparkles: Add ability to paste images

--- js/upload.js
+++ js/upload.js
@@ -9,74 +9,95 @@
 
         const fr = new FileReader();
         fr.onload = () => {
-            if (target.value.match(/\.pdf$/)) {
-                pdfjsLib.getDocument(fr.result).promise.then((pdf) => {
-                    let width = 0;
-                    let height = 0;
-                    const canvases = [];
-
-                    for (let num = 1; num <= pdf.numPages; num++) {
-                        pdf.getPage(num).then((page) => {
-                            const viewport = page.getViewport({scale: 1});
-
-                            const tempCanvas = document.createElement("canvas");
-                            const tempContext = tempCanvas.getContext("2d");
-                            tempCanvas.height = viewport.height;
-                            tempCanvas.width = viewport.width;
-
-                            width = Math.max(width, viewport.width);
-                            height += viewport.height;
-
-                            const renderContext = {
-                                canvasContext: tempContext,
-                                viewport: viewport
-                            };
-
-                            page.render(renderContext).promise.then(() => {
-                                canvases.push({ canvas: tempCanvas, num: num });
-
-                                if (canvases.length === pdf.numPages) {
-                                    const finalCanvas = document.createElement("canvas");
-                                    const finalContext = finalCanvas.getContext("2d");
-
-                                    finalCanvas.height = height;
-                                    finalCanvas.width = width;
-
-                                    canvases.sort((a, b) => a.num - b.num);
-
-                                    let dy = 0;
-                                    canvases.forEach(canvas => {
-                                        finalContext.drawImage(canvas.canvas, 0, dy);
-                                        dy += canvas.canvas.height;
-                                    });
-
-                                    document.querySelector("#img-display").src = finalCanvas.toDataURL("image/png");
-
-                                    document.querySelector("#img-display").classList.remove("hidden");
-                                    document.querySelector("#upload").classList.add("hidden");
-                                    document.querySelector("#remove").classList.remove("hidden");
-                                    document.querySelector("#lines").classList.remove("hidden");
-
-                                    if (pdf.numPages > 1) {
-                                        document.querySelector("#display").classList.add("multi-page");
-                                    } else {
-                                        document.querySelector("#display").classList.remove("multi-page");
-                                    }
-                                }
-                            });
-                        });
-                    }
-                });
-            } else {
-                document.querySelector("#img-display").src= fr.result;
-                document.querySelector("#img-display").classList.remove("hidden")
-                document.querySelector("#upload").classList.add("hidden");
-                document.querySelector("#remove").classList.remove("hidden");
-                document.querySelector("#lines").classList.remove("hidden");
-                document.querySelector("#display").classList.remove("multi-page");
-            }
+            display(fr, target);
         };
 
         fr.readAsDataURL(files[0]);
     });
+
+    document.addEventListener("paste", e => {
+        e.preventDefault();
+
+        const item = (e.clipboardData || e.originalEvent.clipboardData).items[0];
+
+        if (item.kind === "file") {
+            const fr = new FileReader();
+            const blob = item.getAsFile();
+
+            fr.onload = () => {
+                display(fr, { value: "" });
+            };
+
+            fr.readAsDataURL(blob);
+        }
+    });
+
+    function display(fr, target) {
+        if (target.value.match(/\.pdf$/)) {
+            pdfjsLib.getDocument(fr.result).promise.then(pdf => {
+                let width = 0;
+                let height = 0;
+                const canvases = [];
+
+                for (let num = 1; num <= pdf.numPages; num++) {
+                    pdf.getPage(num).then((page) => {
+                        const viewport = page.getViewport({ scale: 1.4 });
+
+                        const tempCanvas = document.createElement("canvas");
+                        const tempContext = tempCanvas.getContext("2d");
+                        tempCanvas.height = viewport.height;
+                        tempCanvas.width = viewport.width;
+
+                        width = Math.max(width, viewport.width);
+                        height += viewport.height;
+
+                        const renderContext = {
+                            canvasContext: tempContext,
+                            viewport: viewport
+                        };
+
+                        page.render(renderContext).promise.then(() => {
+                            canvases.push({ canvas: tempCanvas, num: num });
+
+                            if (canvases.length === pdf.numPages) {
+                                const finalCanvas = document.createElement("canvas");
+                                const finalContext = finalCanvas.getContext("2d");
+
+                                finalCanvas.height = height;
+                                finalCanvas.width = width;
+
+                                canvases.sort((a, b) => a.num - b.num);
+
+                                let dy = 0;
+                                canvases.forEach(canvas => {
+                                    finalContext.drawImage(canvas.canvas, 0, dy);
+                                    dy += canvas.canvas.height;
+                                });
+
+                                document.querySelector("#img-display").src = finalCanvas.toDataURL("image/png");
+
+                                document.querySelector("#img-display").classList.remove("hidden");
+                                document.querySelector("#upload").classList.add("hidden");
+                                document.querySelector("#remove").classList.remove("hidden");
+                                document.querySelector("#lines").classList.remove("hidden");
+
+                                if (pdf.numPages > 1) {
+                                    document.querySelector("#display").classList.add("multi-page");
+                                } else {
+                                    document.querySelector("#display").classList.remove("multi-page");
+                                }
+                            }
+                        });
+                    });
+                }
+            });
+        } else {
+            document.querySelector("#img-display").src= fr.result;
+            document.querySelector("#img-display").classList.remove("hidden")
+            document.querySelector("#upload").classList.add("hidden");
+            document.querySelector("#remove").classList.remove("hidden");
+            document.querySelector("#lines").classList.remove("hidden");
+            document.querySelector("#display").classList.remove("multi-page");
+        }
+    }
 }
